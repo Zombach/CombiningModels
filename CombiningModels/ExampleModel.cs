f1742511584048681d6bf470a5e9411e09adbe2d
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-02-17 16:03:14 +0300

    Add Example

---
+++ CombiningModels/ExampleModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CombiningModels
+{
+    public class ExampleModel
+    {
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public int Year { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
